cf60aea033357df665d1e28b638f1cb3d0b7e0eb
JS\helf <helf1986@qq.com>
2018-05-07 14:21:55 +0800

    no message

--- test_new_api.py
+++ test_new_api.py
@@ -2,10 +2,10 @@
 # 从api接口导入TradeAccount 交易类
 from api.quant_api import TradeAccount, to_dataframe, to_dict
 from common.settings import HBP_ACCESS_KEY, HBP_SECRET_KEY, BNB_ACCESS_KEY, BNB_SECRET_KEY
+import time
 
 # 创建火币交易账户
 hbaccount = TradeAccount(exchange='hbp', api_key=HBP_ACCESS_KEY, api_secret=HBP_SECRET_KEY, currency='USDT')
-
 
 # 通过账户访问行情数据
 bars = hbaccount.get_last_bars(symbol_list='btcusdt, ethusdt', bar_type='1min')
@@ -13,16 +13,19 @@ bars_dict = to_dict(bars[0])
 bars_df = to_dataframe(bars)
 print(bars_df)
 
+time.sleep(5)
 bars = hbaccount.get_bars(symbol_list='btcusdt', bar_type='1min', size=30)
 bars_dict = to_dict(bars[0])
 bars_df = to_dataframe(bars)
 print(bars_df)
 
+time.sleep(3)
 bars = hbaccount.get_bars(symbol_list='ethusdt', bar_type='1min', size=30)
 bars_dict = to_dict(bars[0])
 bars_df = to_dataframe(bars)
 print(bars_df)
 
+time.sleep(1)
 bars = hbaccount.get_bars(symbol_list='bchusdt', bar_type='1min', size=30)
 bars_dict = to_dict(bars[0])
 bars_df = to_dataframe(bars)
