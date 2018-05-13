febb61be4c0448d98c46e52bede8ae80bc6f5a6e
JS\helf <helf1986@qq.com>
2018-05-13 23:10:27 +0800

    no message

--- test_fund_eval.py
+++ test_fund_eval.py
@@ -8,7 +8,7 @@ import api.connection as conn
 from api.fund_perform_eval import PerformEval
 from api import logger
 
-receivers = ['helf1986@qq.com'] # , 'zhaoyu@zhenfund.com', 'ady.chen@icloud.com']  # 收件人邮箱账号
+receivers = ['helf1986@qq.com', 'zhaoyu@zhenfund.com', 'ady.chen@icloud.com']  # 收件人邮箱账号
 
 def clearing(account=None, interval='1day', ctime='00:00:00'):
     '''
@@ -41,7 +41,7 @@ def clearing(account=None, interval='1day', ctime='00:00:00'):
 
     pos_df = pos_df[pos_df['netvol'] != 0]
     pos_df.index = pos_df['sec_id']
-    print(pos_df)
+    # print(pos_df)
 
     # 获取每只币种当前价格
     sec_price = {}
@@ -53,30 +53,31 @@ def clearing(account=None, interval='1day', ctime='00:00:00'):
         each_sec = each.sec_id.replace(currency, '')
         sec_price[each_sec] = each.last_price
 
-    print(sec_price)
+    # print(sec_price)
 
     pos_df['price'] = [sec_price[each] for each in pos_df.index]
     pos_df['netamt'] = pos_df['netvol']*pos_df['price']
-    print(pos_df)
+    # print(pos_df)
 
     pos_result = pos_df[['account_type', 'available', 'frozen', 'loan', 'interest', 'netvol', 'price', 'netamt']]
     pos_result.columns = ['账户类型', '可用', '冻结', '待还借贷', '待还利息', '净持仓量', '当前价格', '净额']
-    print("当前持仓明细：")
-    print(pos_result)
+    # print("当前持仓明细：")
+    # print(pos_result)
+
     total_amount = pos_result['净额'].sum()
     netvalue = total_amount/initial_amount
-    print("当前总持仓额为：", total_amount)
-    print("当前净值为：", netvalue)
+    # print("当前总持仓额为：", total_amount)
+    # print("当前净值为：", netvalue)
 
     nowstr = time.strftime('%Y-%d-%m %H:%M:%S', time.localtime(time.time()))
     pos_result.to_csv('log/当前持仓明细_' + nowstr + ".csv")
 
     # 将结果发送给客户
-    subject = "BitQuant 净值报告：当前总持仓额=%f，单位净值=%f" %(round(total_amount, 4), round(netvalue, 4))
+    subject = "BitQuant 净值报告：当前总持仓额=%.4f，单位净值=%.4f" %(total_amount, netvalue)
 
-    pos_msg = "最新报告时间：" + nowstr
+    pos_msg = "最新报告时间：" + nowstr + "\n"
     pos_msg = pos_msg + "当前总持仓额=%f，单位净值=%f \n" %(round(total_amount, 4), round(netvalue, 4))
-    pos_msg = pos_msg + "当前持仓明细" + "\n"
+    pos_msg = pos_msg + "\n" + "当前持仓明细" + "\n"
     for nn in range(len(pos_result)):
         sec_id = pos_result.index[nn]
         data = pos_result.iloc[nn]
@@ -86,9 +87,8 @@ def clearing(account=None, interval='1day', ctime='00:00:00'):
         elif account_type =='margin':
             account_type = '杠杆账户'
 
-        pos_msg = pos_msg + "%s 账户持有 %s, 可用 %f, 冻结 %f, 待还借贷 %f, 待还利息 %f, 净持仓量 %f, 当前价格 %f, 总金额 %f \n" \
-                  % (account_type, sec_id, round(data['可用'],4), round(data['冻结'], 4), round(data['待还借贷'],4), \
-                     round(data['待还利息'],4), round(data['净持仓量'], 4), round(data['当前价格'],4), round(data['净额'],4))
+        pos_msg = pos_msg + "%s 持有 %s, 可用 %.4f, 冻结 %.4f, 待还借贷 %.4f, 待还利息 %.4f, 净持仓量 %.4f, 当前价格 %.4f, 总金额 %.4f \n" \
+                  % (account_type, sec_id, data['可用'], data['冻结'], data['待还借贷'], data['待还利息'], data['净持仓量'], data['当前价格'], data['净额'])
 
     res = logger.send_mail(subject=subject, content=pos_msg, receivers=receivers)
     if res == True:
