62f7fbda6f176d84ed32814a469713e85d1db23f
JS\helf <helf1986@qq.com>
2018-05-13 22:00:06 +0800

    统计当日持仓

--- test_fund_eval.py
+++ test_fund_eval.py
@@ -43,62 +43,60 @@ def clearing(account=None, interval='1day', ctime='00:00:00'):
 
 if __name__ == '__main__':
 
-    
+    initial_amount = 100000
     hbaccount = TradeAccount(exchange='hbp', api_key=HBP_ACCESS_KEY,api_secret=HBP_SECRET_KEY, currency='USDT')
     currency = hbaccount.currency.lower()
-
-    pos_df = pd.DataFrame([], columns=['spot', 'margin', 'loan'])
+    pos_df = pd.DataFrame([], columns=['type', 'available', 'frozen', 'loan', 'interest', 'netvol', 'price', 'netamt'])
 
     # 普通账户数量
     positions = hbaccount.get_positions(source='spot')
     for each_pos in positions:
         sec_id = each_pos.sec_id.lower()
-        if sec_id in pos_df.index:
-            pos_df.loc[sec_id, 'spot'] = each_pos.volume
-        else:
-            each_df = pd.DataFrame([], index=[sec_id], columns=['spot', 'margin', 'loan'])
-            each_df.loc[sec_id, 'spot'] = each_pos.volume
-            pos_df = pos_df.append(each_df)
-
+        each_df = pd.DataFrame([], index=[sec_id], columns=['spot', 'margin', 'loan'])
+        pos_df.loc[sec_id, 'type'] = 'spot'
+        pos_df.loc[sec_id, 'available'] = each_pos.available
+        pos_df.loc[sec_id, 'frozen'] = each_pos.order_frozen
+        pos_df.loc[sec_id, 'loan'] = 0.0
+        pos_df.loc[sec_id, 'interest'] = 0.0
+        pos_df = pos_df.append(each_df)
 
     # 借贷账户数量
     positions = hbaccount.get_positions(source='margin')
     for each_pos in positions:
         sec_id = each_pos.sec_id.lower()
-        if sec_id in pos_df.index:
-            pos_df.loc[sec_id, 'margin'] = each_pos.volume
-        else:
-            each_df = pd.DataFrame([], index=[sec_id], columns=['spot', 'margin', 'loan'])
-            each_df.loc[sec_id, 'margin'] = each_pos.volume
-            pos_df = pos_df.append(each_df)
-
-    '''
-    # 借贷资产需要扣除
-    for each_sec in pos_df.index:
-        sec_id = each_sec.lower()
-        symbol = sec_id + currency
-        loans = hbaccount.get_margin_balance(symbol=symbol)
-
-    '''
-
-    pos_df = pos_df.fillna(0)
-    pos_df['volume'] = pos_df['spot'] + pos_df['margin'] - pos_df['loan']
-    pos_df['price'] = 0
+        print(sec_id)
+        each_df = pd.DataFrame([], index=[sec_id], columns=['spot', 'margin', 'loan'])
+        pos_df.loc[sec_id, 'type'] = 'margin'
+        pos_df.loc[sec_id, 'available'] = each_pos.available
+        pos_df.loc[sec_id, 'frozen'] = each_pos.order_frozen
+        pos_df.loc[sec_id, 'loan'] = 0.0
+        pos_df.loc[sec_id, 'interest'] = 0.0
+        pos_df = pos_df.append(each_df)
 
     # 统计总的持仓
-    total_amount = 0
-    for each_sec in pos_df.index:
-        sec_id = each_sec.lower()
-        if sec_id != currency:
-            symbol = sec_id + currency
-            tick = hbaccount.get_last_ticks(symbol_list=symbol)
-            symbol_tick = tick[0]
-            print(symbol, symbol_tick.last_price)
-            pos_df.loc[each_sec, 'price'] = symbol_tick.last_price
-        else:
-            pos_df.loc[each_sec, 'price'] = 1
+    pos_df = pos_df.fillna(0)
+    pos_df['netvol'] = pos_df['available'] + pos_df['frozen'] - pos_df['loan'] - pos_df['interest']
+    pos_df['price'] = 0
 
-    pos_df['total'] = pos_df['volume']*pos_df['price']
+    # 获取每只币种当前价格
+    sec_price = {}
+    sec_price[currency] = 1
+    sec_ids = list(set(np.unique(pos_df.index)) - set([currency]))
+    symbols = [each + currency for each in sec_ids]
+    sec_ticks = hbaccount.get_last_ticks(','.join(symbols))
+    for each in sec_ticks:
+        each_sec = each.sec_id.replace(currency, '')
+        sec_price[each_sec] = each.last_price
+
+    print(sec_price)
+
+    pos_df['price'] = [sec_price[each] for each in pos_df.index]
+    pos_df['netamt'] = pos_df['netvol']*pos_df['price']
+
+
+    pos_df.columns = ['账户类型', '可用', '冻结', '待还借贷', '待还利息', '净持仓量', '当前价格', '净额']
+    print("当前持仓明细：")
     print(pos_df)
-    total_amount = pos_df['total'].sum()
-    print(total_amount)
+    total_amount = pos_df['净额'].sum()
+    print("当前总持仓额为：", total_amount)
+    print("当前净值为：", total_amount/initial_amount)
