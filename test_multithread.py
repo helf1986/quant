b386a2406ecc5cb010eae376d1b917b61888de68
JS\helf <helf1986@qq.com>
2018-05-17 20:33:21 +0800

    no message

--- test_multithread.py
+++ test_multithread.py
@@ -4,8 +4,10 @@ import threading
 import time
 from websocket import create_connection
 import gzip
+from queue import Queue
 
 exitFlag = 0
+from api.quant_api import Bar
 
 class myThread (threading.Thread):
 
@@ -20,8 +22,12 @@ class myThread (threading.Thread):
         print_time(self.name, self.counter, 5)
         print ("退出线程：" + self.name)
 
+QUEUE_SIZE = 2000
+bar_queue = Queue(maxsize=QUEUE_SIZE)
+last_bar = Bar()
+bar_list = []
 
-class Subscribe_Bars (threading.Thread):
+class Subscribe_Bars(threading.Thread):
 
     def __init__(self, exchange, symbol, type):
         threading.Thread.__init__(self)
@@ -51,8 +57,43 @@ class Subscribe_Bars (threading.Thread):
                 pong = '{"pong":' + ts + '}'
                 ws.send(pong)
                 ws.send(tradeStr)
-            else:
-                print(result)
+            elif result[2:4] == "ch":
+                # print(result)
+                new_bar = Bar()
+                data = result['tick']
+                new_bar.utc_time = data['ts']
+                structtime = time.localtime(data['ts'])
+                new_bar.strtime = time.strftime('%Y-%m-%d %H:%M:%S', structtime)
+                new_bar.open = data['open']
+                new_bar.high = data['high']
+                new_bar.low = data['low']
+                new_bar.close = data['close']
+                new_bar.volume = data['amount']
+                new_bar.amount = data['volume']
+
+                # 每分钟保留一个数据
+
+                if last_bar.utc_time != new_bar.utc_time:
+                    bar_queue.put(new_bar)
+
+
+class Strategy(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+
+        while(1):
+            if bar_queue.qsize() > 0:
+                new_bar = bar_queue.get()
+                bar_list = bar_list + [new_bar]
+                bar_list = bar_list[-QUEUE_SIZE:]
+                print(len(bar_list))
+                print(new_bar.strtime, new_bar.close)
+
+
+
 
 
 def print_time(threadName, delay, counter):
@@ -65,10 +106,12 @@ def print_time(threadName, delay, counter):
 
 # 创建新线程
 thread1 = Subscribe_Bars(exchange='hbp', symbol='btcusdt', type='1min')
-thread2 = myThread(2, "Thread-2", 2)
+thread2 = Strategy()
 
 # 开启新线程
 thread1.start()
+thread2.start()
+
 # thread1.join()
 
 # thread2.start()
