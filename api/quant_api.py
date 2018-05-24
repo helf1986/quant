576549b3163ed1350097a04c85480f0fbb65844c
JS\helf <helf1986@qq.com>
2018-05-24 18:50:58 +0800

    no message

--- api/quant_api.py
+++ api/quant_api.py
@@ -397,7 +397,7 @@ class TradeAccount(object):
                         depth_df.sort_values(by='price', ascending=False, inplace=True)
                         '''
 
-                        print(depth_df[['symbol', 'strtime', 'ask_price', 'ask_volume', 'bid_price', 'bid_volume']])
+                        print(depth_df[['symbol', 'strtime', 'ask_price', 'ask_volume', 'bid_price', 'bid_volume']].head())
                         if queue.qsize() == QUEUE_SIZE:
                             queue.get()
                         queue.put(depth_df)
